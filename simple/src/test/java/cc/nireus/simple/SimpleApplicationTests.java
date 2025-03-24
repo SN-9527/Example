9052c3af68cbcc95c7cb7dd8ded5a862f8349063
Supreme <github@nireus.cc>
2025-03-24 18:28:49 +0800

    update

---
+++ simple/src/test/java/cc/nireus/simple/SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.nireus.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
