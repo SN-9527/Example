3a182deadb8dc8adaf2f1d7ca411655d07d75fd2
Supreme <github@nireus.cc>
2025-03-24 18:16:21 +0800

    update

---
+++ simplebootproject/src/main/java/cc/nireus/simplebootproject/SimplebootprojectApplication.java
@@ -0,0 +1,13 @@
+package cc.nireus.simplebootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebootprojectApplication.class, args);
+	}
+
+}
---
+++ simplebootproject/src/test/java/cc/nireus/simplebootproject/SimplebootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.nireus.simplebootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
